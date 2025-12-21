f6c184ac7ff8e02f8ad2f5acbc291dea98b1923a
Buddha <patholitm@gmail.com>
2025-12-21 07:04:06 -0500

    Initial Commit

---
+++ 11/_1App.swift
@@ -0,0 +1,32 @@
+//
+//  _1App.swift
+//  11
+//
+//  Created by Patrick on 12/21/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct _1App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ 11Tests/_1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  _1Tests.swift
+//  11Tests
+//
+//  Created by Patrick on 12/21/25.
+//
+
+import Testing
+
+struct _1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
